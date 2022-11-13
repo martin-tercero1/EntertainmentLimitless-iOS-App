a3dbf7b42dc6acaaf3c08a40b844a4e85816ab70
Duilio Rosciano <drosciano@harding.edu>
2022-11-13 17:32:01 -0600

    Initial Commit

---
+++ EntertainmentLimitless/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EntertainmentLimitless
+//
+//  Created by Duilio Rosciano on 11/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EntertainmentLimitless/EntertainmentLimitlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainmentLimitlessApp.swift
+//  EntertainmentLimitless
+//
+//  Created by Duilio Rosciano on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct EntertainmentLimitlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
